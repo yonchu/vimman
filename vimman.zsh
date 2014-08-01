7d76dd0e29e4aed02f2a7ee3b3b5db80d17acb21
Christian Storm <Christian.Storm@tngtech.com>
2014-08-01 21:07:08 +0200

    follow symlinks in search of doc directories

--- vimman.zsh
+++ vimman.zsh
@@ -88,7 +88,7 @@ function vimman() {
         if [[ ! -d $dir ]]; then
             continue
         fi
-        targets+=(${(f)"$(find "$dir" -type d -name '.neobundle' -prune -o -type f -name "$1" -print)"})
+        targets+=(${(f)"$(find -L "$dir" -type d -name '.neobundle' -prune -o -type f -name "$1" -print)"})
     done
 
     if [[ -d ~/.vim/doc ]]; then
@@ -155,7 +155,7 @@ function _vimman_get_help_files() {
         if [[ ! -d $dir ]]; then
             continue
         fi
-        doc+=(${(f)"$(find "$dir" -type d -name 'doc')"})
+        doc+=(${(f)"$(find -L "$dir" -type d -name 'doc')"})
     done
 
     if [[ -d ~/.vim/doc ]]; then
